45a92fde9acf8b8a3d5582a29cc3b16bddf8b7e0
Anshika Jain <43791442+Anshika-Jain@users.noreply.github.com>
2019-04-22 22:57:51 +0530

    Add files via upload

---
+++ vowelorconsonant.py
@@ -0,0 +1,8 @@
+#check wether a character is a vowel or a consonant
+print("Enter a alphabetical character ")
+ch=input()
+if ch in ('a','A','e','E','i','I','o','O','u','U'):
+    print("Its a vowel")
+else:
+    print("Its a consonant")
+    
